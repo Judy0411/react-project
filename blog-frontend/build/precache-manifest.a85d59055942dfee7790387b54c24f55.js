661b0d418eda686bced77f0f2eb1a1c3d4658a1b
Yeonsoo Seo <ys.seo@greenit.cc>
2022-12-14 11:31:37 +0900

    build 폴더 추가

---
+++ blog-frontend/build/precache-manifest.a85d59055942dfee7790387b54c24f55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36a67a8d9a99b694c684eae6617e8c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b584560d506f2abb603",
+    "url": "/static/css/2.d15af080.chunk.css"
+  },
+  {
+    "revision": "83c2c2fb127c9b106837",
+    "url": "/static/css/main.0d804af2.chunk.css"
+  },
+  {
+    "revision": "1b584560d506f2abb603",
+    "url": "/static/js/2.46c7334b.chunk.js"
+  },
+  {
+    "revision": "83c2c2fb127c9b106837",
+    "url": "/static/js/main.0a112ea0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
